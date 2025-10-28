b41477572d682314f4ed97b94ac9e1e8a267ea72
Nicolas Lucero <lucero.nico216@gmail.com>
2025-10-28 12:07:59 -0300

    Cambios minimos en la estructura

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import CardTest from "./CardTest";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      {/* Navbar */}
+    <div className="bg-primary font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Navbar menuItems={["Proyectos", "Sobre mí", "Contacto"]} />
       {/* Hero Section: Hero → CTA: “Ver proyectos” / “Contactarme” (CTA es call to action) */}
       {/* About Me (mini) + Tech stack (iconitos) y podria ver un link a un About Extendido con Bio extendida, certificaciones*/}
       {/* Proyectos Destacados : destacados (3–6) */}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = ({menuItems}) => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <a className="btn btn-ghost text-xl">daisyUI</a>
+            
+        </div>
+    )
+}
+
+export default Navbar

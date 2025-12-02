71a44e6b767724a69cd6e0d1fe29b3882c5df492
Nicolas Lucero <lucero.nico216@gmail.com>
2025-12-02 12:56:33 -0300

    cambios en el theme de daisy. Se agrego el theme controller

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" data-theme="night">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

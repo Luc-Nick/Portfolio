10c70e08924ac2302e04d6e510146a3d1b012f76
Nicolas Lucero <lucero.nico216@gmail.com>
2025-12-09 12:53:56 -0300

    Refactor Navbar component structure and update branding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Navbar = ({menuItems}) => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <a className="btn btn-ghost text-xl">daisyUI</a>
-            
+        <div className="navbar rounded-3xl bg-base-100 shadow-sm">
+            <div className='navbar-start'>
+                <a className="btn btn-ghost text-xl">Nick</a>
+            </div>
+            <div className='navbar-center'>
+                <ul className="menu menu-horizontal px-1 gap-6">
+                    {menuItems.map((item, index) => (
+                        <li key={index}><a>{item}</a></li>
+                    ))}
+                </ul>
+            </div>
+            <div className='navbar-end'>
+                <a className="btn">Descargar CV</a>
+            </div>
         </div>
     )
 }
